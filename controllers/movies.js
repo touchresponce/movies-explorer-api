1ad953ad1b8f49c29358e83825e3f8598557c5d2
touchresponce <touchresponce71@gmail.com>
2022-11-06 17:46:10 +0300

    fix: The Little Engine That Could

--- controllers/movies.js
+++ controllers/movies.js
@@ -1,7 +1,7 @@
-const Movie = require('../models/movie');
-const NotFound = require('../errors/NotFoundError');
-const ForbiddenError = require('../errors/ForbiddenError');
-const BadRequest = require('../errors/BadRequest');
+const Movie = require("../models/movie");
+const NotFound = require("../errors/NotFoundError");
+const ForbiddenError = require("../errors/ForbiddenError");
+const BadRequest = require("../errors/BadRequest");
 
 // возвращает все сохранённые текущим  пользователем фильмы
 module.exports.getMovies = (req, res, next) => {
@@ -12,39 +12,12 @@ module.exports.getMovies = (req, res, next) => {
 
 // создаёт фильм
 module.exports.postMovie = async (req, res, next) => {
-  const {
-    country,
-    director,
-    duration,
-    year,
-    description,
-    image,
-    trailerLink,
-    nameRU,
-    nameEN,
-    thumbnail,
-    movieId,
-  } = req.body;
-
   try {
-    const movie = await Movie.create({
-      country,
-      director,
-      duration,
-      year,
-      description,
-      image,
-      trailerLink,
-      nameRU,
-      nameEN,
-      thumbnail,
-      movieId,
-      owner: req.user._id,
-    });
+    const movie = await Movie.create({ ...req.body, owner: req.user._id });
     res.send(movie);
   } catch (err) {
-    if (err.name === 'ValidationError') {
-      next(new BadRequest('Переданы некорректные данные при создании.'));
+    if (err.name === "ValidationError") {
+      next(new BadRequest("Переданы некорректные данные при создании."));
     } else {
       next(err);
     }
@@ -56,14 +29,14 @@ module.exports.deleteMovie = (req, res, next) => {
   const { movieId } = req.params;
   Movie.findById(movieId)
     .orFail(() => {
-      throw new NotFound('Фильм с указанным _id не найден');
+      throw new NotFound("Фильм с указанным _id не найден");
     })
     .then((movie) => {
       if (!movie.owner.equals(req.user._id)) {
-        return next(new ForbiddenError('Ошибка доступа'));
+        return next(new ForbiddenError("Ошибка доступа"));
       }
       return movie.remove().then(() => {
-        res.send({ message: 'Фильм удален' });
+        res.send({ message: "Фильм удален" });
       });
     })
     .catch(next);
