00549970f4ed4052b527457ad34f211bd3a5a761
touchresponce <touchresponce71@gmail.com>
2022-11-24 21:17:23 +0300

    test

--- middlewares/rateLimiter.js
+++ middlewares/rateLimiter.js
@@ -1,8 +1,8 @@
-const rateLimit = require('express-rate-limit');
+const rateLimit = require("express-rate-limit");
 
 const limiter = rateLimit({
   windowMs: 5 * 60 * 1000, // 5 minutes
-  max: 100, // limit each IP to 100 requests per windowMs
+  max: 200, // limit each IP to 200 requests per windowMs
 });
 
 module.exports = limiter;
